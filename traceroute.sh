d2e9fbbb989996946a79637aed4e27c165650974
smskin <serg@msoinvest.com>
2021-12-17 11:15:12 +0300

    init

--- traceroute.sh
+++ traceroute.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+
 TRACEROUTE_HOST=$(cat /root/TRACEROUTE_HOST)
+#TRACEROUTE_HOST=ya.ru
 
 if [[ -z "${TRACEROUTE_HOST}" ]]; then
   echo "Some default value because TRACEROUTE_HOST is undefined"
   exit
 fi
 
+DT=$(date +%Y-%m-%dT%H:%M:%S%z)
 START=$(date +%s)
 OUTPUT=$(traceroute -w 10 "${TRACEROUTE_HOST}" |tr "\n" "," )
 END=$(date +%s)
 EXECUTION_TIME=$(( $END - $START ))
-printf "{signature:\"traceroute\",target:\"%s\"start:%d,end:%d,execution_time:%d,output:\"%s\"}" "$TRACEROUTE_HOST" "$START" "$END" "$EXECUTION_TIME" "$OUTPUT"
+echo "{@t:\"$DT\",@m:\"traceroute\",@l:\"INFO\",@s:\"traceroute\",target:\"$TRACEROUTE_HOST\",start:$START,end:$END,execution_time:$EXECUTION_TIME,output:\"$OUTPUT\"}"
